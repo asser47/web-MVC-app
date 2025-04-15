298039bd15f3055e176b16e374d922154bf55cd3
asser47 <asseryehia12@gmail.com>
2025-04-15 10:49:11 +0200

    Remove all projects except StudentPortal.Web

--- WebApplication1/WebApplication1/AttachmentOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication1
-{
-    public class AttachmentOptions
-    {
-           public string AllowedExtensions { get; set; }
-           public int MaxSizeinMegaBytes { get; set; }
-           public bool EnableCompression { get; set; }
-    }
-}
--- WebApplication1/WebApplication1/Data/UserPermission.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebApplication1.Data
-{
-    public class UserPermission
-    {
-        public int UserId { get; set; }
-        public Permission PermissionsId { get; set; }
-    }
-}
